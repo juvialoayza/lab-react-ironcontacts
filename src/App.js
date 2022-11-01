feda18642897c8f90bb0900d0d8a1af5c7f12fd1
Juvixa A <juvixa.aloayza@gmail.com>
2022-11-01 23:16:36 +0100

    iteration 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsArr from "./contacts.json"
+import { useState } from "react"
 
 function App() {
+  
+  const [contacts, setContacts] = useState(contactsArr.slice(0, 5))
+  
+  // const awards =  "🏆"
+  
+
+  const addContact = () => {
+    
+    const randomIndex = Math.floor(Math.random()*contactsArr.length)
+    const randomContact = contactsArr[randomIndex]
+
+    const copy=[...contacts]
+    copy.push(randomContact)
+
+    setContacts(copy)
+  }
+
+  const contactOrderByPopularity = () =>{
+    const copy = contacts.map((elem)=> elem)
+    
+    copy.sort((elem1,elem2)=>{
+     if (elem1.popularity > elem2.popularity){
+      return 1
+    }else{
+      return -1
+    }
+    })
+    setContacts(copy)
+  }
+
+  const contactOrderByName = () =>{
+    const copy = contacts.map((elem)=> elem)
+    
+    copy.sort((elem1,elem2)=>{
+     if (elem1.name > elem2.name){
+      return 1
+    }else{
+      return -1
+    }
+    })
+    setContacts(copy)
+  }
+
+  const deleteContact = (contactId) => {
+   
+    const filteredContacts = contacts.filter((eachContact)=> {
+      if(eachContact.id === contactId){
+      return false
+    } else {
+        return true
+      }
+    })
+
+    setContacts(filteredContacts)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+
+      <button onClick={addContact}>Add Random Contact</button>
+      <button onClick={contactOrderByPopularity}>Sort by popularity</button>
+      <button onClick={contactOrderByName}>Sort by name</button>
+
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+
+        {contacts.map((eachContact) => {
+          return (
+            <tbody>
+              <tr>
+                <td><img style={{ width: '50px' }} src={eachContact.pictureUrl} alt="" /></td>
+                <td>{eachContact.name}</td>
+                <td>{(eachContact.popularity).toFixed(2)}</td>
+                <td>{(eachContact.wonOscar)=== true ? "🏆": ""}</td>
+                <td>{(eachContact.wonEmmy)}</td>
+              <button onClick={()=> deleteContact(eachContact.id)}>Delete</button>
+              </tr>
+            </tbody>
+          )
+        })}
+      </table>
+
+
+
     </div>
+
   );
 }
+
 
 export default App;
